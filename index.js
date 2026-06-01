092ff36e265c25ff14d51c6ab9591477f8591566
Tarek Mariscal <tarekmrscl@gmail.com>
2026-06-01 17:57:41 -0600

    chore: configure ejs-mate as view engine

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const port = 3000
 const mongoose = require('mongoose');
+const engine = require('ejs-mate')
 const methodOverride = require('method-override')
 
 const Auto = require('./models/auto')
@@ -17,6 +18,7 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
