c557ad47a30fbc8acba3d6d6ae79fc40d9ee4c62
Tarek Mariscal <tarekmrscl@gmail.com>
2026-06-03 15:19:51 -0600

    fix: correct image URLs in car seed data

--- seeds/seedData.js
+++ seeds/seedData.js
@@ -29,7 +29,7 @@ module.exports.seedAutos = [
         price: 1684,
         carClass: 920,
         country: 'Italy',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://global.discourse-cdn.com/forza/original/4X/e/9/2/e923a75107c128576e1a5dc03d444db010337606.jpeg'
     },
     {
         name: '2013 Audi RS 4 Avant',
@@ -37,7 +37,7 @@ module.exports.seedAutos = [
         price: 487,
         carClass: 607,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://i.ytimg.com/vi/G5lyREP4A80/maxresdefault.jpg'
     },
     {
         name: '2022 BMW M5 CS',
@@ -45,7 +45,7 @@ module.exports.seedAutos = [
         price: 842,
         carClass: 731,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://preview.redd.it/the-car-i-most-want-to-be-in-fh5-is-the-2022-bmw-m5-cs-lmk-v0-ybz1jc4czsg71.jpg?width=1280&format=pjpg&auto=webp&s=d7848aa49000c82a0976499a166efa7f73f23c53'
     },
     {
         name: '2018 Porsche Cayenne Turbo',
@@ -53,7 +53,7 @@ module.exports.seedAutos = [
         price: 294,
         carClass: 638,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://gtspirit.com/wp-content/uploads/2017/11/Porsche-Cayenne-Turbo-4-1-1068x707.jpg'
     },
     {
         name: '2024 Chevrolet Corvette E-Ray',
@@ -61,7 +61,7 @@ module.exports.seedAutos = [
         price: 915,
         carClass: 737,
         country: 'USA',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://i.ytimg.com/vi/UEoRewi_lF0/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGGUgXChSMA8=&rs=AOn4CLDyAA0ma2fQSZNWvE879bvnHdmeVg'
     },
     {
         name: '1984 Audi Sport quattro',
@@ -69,7 +69,7 @@ module.exports.seedAutos = [
         price: 158,
         carClass: 526,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://preview.redd.it/recreated-the-audi-sport-quattro-in-which-mich%C3%A8le-mouton-v0-9gd3b8pr2kz71.jpg?width=1080&crop=smart&auto=webp&s=64fc5be4749132ea74805f6932e249f200b2482f'
     },
     {
         name: '2019 Ferrari F8 Tributo',
@@ -77,7 +77,7 @@ module.exports.seedAutos = [
         price: 723,
         carClass: 802,
         country: 'Italy',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://i.ytimg.com/vi/WifrOc3NjPg/maxresdefault.jpg'
     },
     {
         name: '2021 Porsche Mission R',
@@ -85,7 +85,7 @@ module.exports.seedAutos = [
         price: 1248,
         carClass: 865,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/2021/Motorsports/IAA/Exterior/Gallery-1/MicrosoftTeams-image-(4).jpeg/jcr:content/MicrosoftTeams-image%20(4).jpeg'
     },
     {
         name: '2022 Aston Martin Valkyrie AMR Pro',
@@ -93,7 +93,7 @@ module.exports.seedAutos = [
         price: 1937,
         carClass: 989,
         country: 'UK',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://i.ytimg.com/vi/r0xLa46vrX0/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBUvsG8AK1LnBMXqK_BYvPQCzeEiA'
     },
     {
         name: '2023 BMW M2 Forza Edition',
@@ -101,7 +101,7 @@ module.exports.seedAutos = [
         price: 376,
         carClass: 700,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://preview.redd.it/finally-got-the-m2-forza-edition-v0-gsi41hnutp1h1.jpeg?width=1080&crop=smart&auto=webp&s=7879602a7d2eba8f273056a58f49b350a7e607d5'
     },
     {
         name: '2017 Ferrari J50',
@@ -109,7 +109,7 @@ module.exports.seedAutos = [
         price: 657,
         carClass: 772,
         country: 'Italy',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://i.redd.it/2017-ferrari-j50-the-new-car-pass-vehicle-of-fh5-v0-biaeo8nc4l481.jpg?width=1920&format=pjpg&auto=webp&s=54ac049090645e6885319371b9d90d9426378b07'
     },
     {
         name: '2019 Porsche 911 Carrera S',
@@ -117,6 +117,6 @@ module.exports.seedAutos = [
         price: 531,
         carClass: 714,
         country: 'Germany',
-        image: 'https://example.com/car-image.jpg'
+        image: 'https://a.storyblok.com/f/322327/3840x1266/cd758f4c72/cz26w03ox0004-carrera-s-desktop.jpg/m/2560x822/smart/filters:format(webp)?dpl=dpl_FFCXzWQk3dMf7U5zNBWvpbzr2JKF'
     }
 ]
